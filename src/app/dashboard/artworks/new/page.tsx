414bb9d9f381a62eaf5a7ec4570a2a8fc8c03bd1
kehqan <154426011+kehqan@users.noreply.github.com>
2026-03-26 11:30:36 +0100

    Delete src directory

--- src/app/dashboard/artworks/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import ArtworkForm from "@/components/artworks/ArtworkForm";
-
-export default function NewArtworkPage() {
-  return (
-    <div className="max-w-3xl">
-      <ArtworkForm />
-    </div>
-  );
-}
